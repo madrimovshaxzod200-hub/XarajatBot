8a98f122fcf814a5a04e45dd13c84b8d9e1a2107
madrimovshaxzod200-hub <madrimovshaxzod200@gmail.com>
2026-02-13 14:32:26 +0500

    Create database.py

---
+++ database.py
@@ -0,0 +1,68 @@
+import aiosqlite
+import datetime
+
+DB_NAME = "database.db"
+
+
+async def create_tables():
+    async with aiosqlite.connect(DB_NAME) as db:
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS users(
+            user_id INTEGER PRIMARY KEY,
+            username TEXT,
+            created_at TEXT
+        )
+        """)
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS expenses(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            amount INTEGER,
+            category TEXT,
+            date TEXT,
+            time TEXT
+        )
+        """)
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS categories(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            name TEXT,
+            usage_count INTEGER DEFAULT 1
+        )
+        """)
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS reminders(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            time TEXT
+        )
+        """)
+
+        await db.commit()
+
+
+async def add_user(user_id, username):
+    async with aiosqlite.connect(DB_NAME) as db:
+
+        cur = await db.execute(
+            "SELECT user_id FROM users WHERE user_id = ?",
+            (user_id,)
+        )
+
+        user = await cur.fetchone()
+
+        if not user:
+            await db.execute(
+                "INSERT INTO users VALUES (?, ?, ?)",
+                (
+                    user_id,
+                    username,
+                    datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+                )
+            )
+            await db.commit()
